75e76872881d8bcc0ac47bbbe0cd6dcda27e70ec
AndyTCF <andy.c.f.98@gmail.com>
2020-11-29 20:29:38 +1100

    add test for register and deregister

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -8,21 +8,22 @@ import "@openzeppelin/contracts/utils/Pausable.sol";
 contract Registry is Ownable, Pausable {
     using Address for address;
 
-    event RegistrationAdded(bytes32 registryId, address owner);
-    event RegistrationRemoved(bytes32 registryId);
+    event RegistrationAdded(uint registryId, address owner);
+    event RegistrationRemoved(uint registryId);
 
-    event RealtyTransferred(bytes32 registryId, address from, address to);
-    event RealtyPurchased(bytes32 registryId, uint price, address purchaser);
-    event RealtyPriceUpdated(bytes32 registryId, uint price);
-    event RealtyStateChanged(bytes32 registryId, RealtyState newState);
+    event RealtyTransferred(uint registryId, address from, address to);
+    event RealtyPurchased(uint registryId, uint price, address purchaser);
+    event RealtyPriceUpdated(uint registryId, uint price);
+    event RealtyStateChanged(uint registryId, RealtyState newState);
 
-    mapping(bytes32 => Realty) public idToRealty;
-    mapping(bytes32 => address) public ownerOf;
-    mapping(address => bytes32[]) public realtyOwned;
-    mapping(bytes32 => uint) public indexOfRealty;
+    mapping(uint => Realty) public idToRealty;
+    mapping(uint => address) public ownerOf;
+    mapping(address => uint[]) public realtyOwned;
+    mapping(uint => uint) public indexOfRealty;
     mapping(address => uint) public balance;
 
     uint public totalRegistrations;
+    uint public currentID;
     uint public registryCreated;
 
     enum RealtyState {Available, NotAvailable}
@@ -33,7 +34,7 @@ contract Registry is Ownable, Pausable {
         string city;
         string country;
         string realtyType;
-        bytes32 registryId;
+        uint registryId;
         uint price;
         RealtyState state;
     }
@@ -42,7 +43,7 @@ contract Registry is Ownable, Pausable {
         registryCreated = now;
     }
 
-    modifier onlyRealtyOwner(bytes32 registryId) {
+    modifier onlyRealtyOwner(uint registryId) {
         require(ownerOf[registryId] == msg.sender, "Msg sender is not the owner of the Realty");
         _;
     }
@@ -55,35 +56,34 @@ contract Registry is Ownable, Pausable {
         string memory realtyType,
         uint price,
         address owner
-    ) public whenNotPaused() onlyOwner() returns (bytes32) {
-        bytes32 registryId = keccak256(abi.encodePacked(msg.sender, streetName, realtyType, now));
-
-        require(ownerOf[registryId] == address(0), "Realty already registered");
-
+    ) public whenNotPaused() onlyOwner() returns (uint) {
         Realty memory registeredRealty = Realty(
             streetName,
             postCode,
             city,
             country,
             realtyType,
-            registryId,
+            currentID,
             price,
             RealtyState.NotAvailable
         );
-        idToRealty[registryId] = registeredRealty;
 
-        _register(registryId, owner);
+        idToRealty[currentID] = registeredRealty;
+
+        _register(currentID, owner);
+
+        currentID++;
 
         return registeredRealty.registryId;
     }
 
-    function deregister(bytes32 registryId) public whenNotPaused() onlyOwner() {
+    function deregister(uint registryId) public whenNotPaused() onlyOwner() {
         require(ownerOf[registryId] != address(0), "Realty is not registered");
         _deregister(registryId);
         emit RegistrationRemoved(registryId);
     }
 
-    function purchaseRegistration(bytes32 registryId) public payable whenNotPaused() {
+    function purchaseRegistration(uint registryId) public payable whenNotPaused() {
         Realty memory currentRealty = idToRealty[registryId];
         require(currentRealty.state == RealtyState.Available, "Realty is not for sale");
         require(msg.value >= currentRealty.price, "Insufficient value sent to purchase realty");
@@ -110,21 +110,21 @@ contract Registry is Ownable, Pausable {
         msg.sender.transfer(amount);
     }
 
-    function changePrice(bytes32 registryId, uint price) public whenNotPaused() onlyRealtyOwner(registryId) returns (uint) {
-        require(price > 0, "Method called with an invalid price");
+    function changePrice(uint registryId, uint price) public whenNotPaused() onlyRealtyOwner(registryId) returns (uint) {
+        require(price >= 0, "Method called with an invalid price");
         idToRealty[registryId].price = price;
         emit RealtyPriceUpdated(registryId, price);
         return price;
     }
 
-    function setAvailable(bytes32 registryId) public whenNotPaused() onlyRealtyOwner(registryId) {
+    function setAvailable(uint registryId) public whenNotPaused() onlyRealtyOwner(registryId) {
         RealtyState currentState = idToRealty[registryId].state;
         require(currentState != RealtyState.Available, "Realty state already available");
         idToRealty[registryId].state = RealtyState.Available;
         emit RealtyStateChanged(registryId, RealtyState.Available);
     }
 
-    function setNotAvailable(bytes32 registryId) public whenNotPaused() onlyRealtyOwner(registryId) {
+    function setNotAvailable(uint registryId) public whenNotPaused() onlyRealtyOwner(registryId) {
         RealtyState currentState = idToRealty[registryId].state;
         require(currentState != RealtyState.NotAvailable, "Realty state already not available");
         idToRealty[registryId].state = RealtyState.NotAvailable;
@@ -134,7 +134,7 @@ contract Registry is Ownable, Pausable {
     function _transferOwner(
         address from,
         address to,
-        bytes32 registryId
+        uint registryId
     ) private {
         require(from != address(0), "Method called with the zero address");
         require(to != address(0), "Method called with the zero address");
@@ -148,10 +148,10 @@ contract Registry is Ownable, Pausable {
         emit RealtyTransferred(registryId, from, to);
     }
 
-    function _register(bytes32 registryId, address owner) private {
+    function _register(uint registryId, address owner) private {
         ownerOf[registryId] = owner;
 
-        bytes32[] storage previousRealtyOwned = realtyOwned[owner];
+        uint[] storage previousRealtyOwned = realtyOwned[owner];
         uint index = previousRealtyOwned.length;
 
         previousRealtyOwned.push(registryId);
@@ -163,11 +163,11 @@ contract Registry is Ownable, Pausable {
         totalRegistrations++;
     }
 
-    function _deregister(bytes32 registryId) private {
+    function _deregister(uint registryId) private {
         address previousOwner = ownerOf[registryId];
         uint index = indexOfRealty[registryId];
 
-        bytes32[] storage ownedAssets = realtyOwned[previousOwner];
+        uint[] storage ownedAssets = realtyOwned[previousOwner];
 
         ownedAssets[index] = ownedAssets[ownedAssets.length - 1];
 
--- test/registry.js
+++ test/registry.js
@@ -4,13 +4,13 @@ const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test
 
 contract('ResidentialHome', async accounts => {
 	const [ownerAddress, userOne, userTwo, userThree] = accounts;
-
 	const streetName = 'Amphitheatre Parkway';
 	const postCode = '94035';
 	const city = 'Mountain View';
 	const country = 'United States';
-	const realtyId = new BN(0);
 	const realtyType = 'Residential Home';
+	const price = 10000;
+	const registryId = '0';
 
 	describe('Registering', () => {
 		let registry;
@@ -19,39 +19,146 @@ contract('ResidentialHome', async accounts => {
 				from: ownerAddress,
 			});
 		});
-		it('should allow an owner to register', async () => {
-			const tx = await registry.register(
+
+		describe('when called by the contract owner', () => {
+			let tx;
+			beforeEach(async () => {
+				tx = await registry.register(
+					streetName,
+					postCode,
+					city,
+					country,
+					realtyType,
+					price,
+					userOne,
+					{
+						from: ownerAddress,
+					}
+				);
+			});
+			it('should emit a registration event', () => {
+				expectEvent(tx, 'RegistrationAdded', {
+					registryId: registryId,
+					owner: userOne,
+				});
+			});
+			it('should update ownerOf state correctly', async () => {
+				assert.equal(await registry.ownerOf.call(registryId), userOne);
+			});
+			it('should update realtyOwned state correctly', async () => {
+				const ownedRealty = await registry.realtyOwned.call(userOne, 0);
+				assert.equal(ownedRealty.toString(), registryId);
+			});
+			it('should update realtyOwned state correctly when adding more than one registration', async () => {
+				let ownedRealty = await registry.realtyOwned.call(userOne, 0);
+				assert.equal(ownedRealty.toString(), registryId);
+				await registry.register(
+					'Street Name',
+					'22222',
+					'NSW',
+					'AU',
+					'Commercial Business',
+					new BN(100000),
+					userOne,
+					{
+						from: ownerAddress,
+					}
+				);
+				ownedRealty = await registry.realtyOwned.call(userOne, 1);
+				assert.equal(ownedRealty.toString(), '1');
+			});
+			it('should update idtoRealty state correctly', async () => {
+				const registeredRealtyStruct = await registry.idToRealty.call(registryId);
+				assert.equal(registeredRealtyStruct[0], streetName);
+				assert.equal(registeredRealtyStruct[1], postCode);
+				assert.equal(registeredRealtyStruct[2], city);
+				assert.equal(registeredRealtyStruct[3], country);
+				assert.equal(registeredRealtyStruct[4], realtyType);
+				assert.equal(registeredRealtyStruct[6], price.toString());
+			});
+			it('should update indexOfRealty state correctly', async () => {
+				const indexOfRealty = await registry.indexOfRealty.call(registryId);
+				assert.equal(indexOfRealty, 0);
+			});
+			it('should update totalRegistrations state correctly', async () => {
+				const totalRegistrations = await registry.totalRegistrations.call();
+				assert.equal(totalRegistrations, 1);
+			});
+			it('should update currentID state correctly', async () => {
+				const currentID = await registry.currentID.call();
+				assert.equal(currentID, 1);
+			});
+		});
+
+		describe('when not called by the contract owner', () => {
+			it('should revert', async () => {
+				await expectRevert(
+					registry.register(
+						streetName,
+						postCode,
+						city,
+						country,
+						realtyType,
+						price,
+						userOne,
+						{
+							from: userTwo,
+						}
+					),
+					'Ownable: caller is not the owner.'
+				);
+			});
+		});
+	});
+
+	describe('Deregistering', () => {
+		let registry;
+		let tx;
+		beforeEach(async () => {
+			registry = await Registry.new({
+				from: ownerAddress,
+			});
+			await registry.register(
 				streetName,
 				postCode,
 				city,
 				country,
 				realtyType,
+				price,
 				userOne,
 				{
 					from: ownerAddress,
 				}
 			);
-			expectEvent(tx, 'RegistrationAdded', {
-				registryId: realtyId,
-				owner: userOne,
-			});
-			assert.equal(await registry.ownerOf.call(realtyId), userOne);
-			const ownedRealty = await registry.realtyOwned.call(userOne, 0);
-			assert.equal(ownedRealty.length, 1);
-			const registeredRealtyStruct = await registry.idToRealty.call(realtyId);
-			assert.equal(registeredRealtyStruct[0], streetName);
-			assert.equal(registeredRealtyStruct[1], postCode);
-			assert.equal(registeredRealtyStruct[2], city);
-			assert.equal(registeredRealtyStruct[3], country);
-			assert.equal(registeredRealtyStruct[4], realtyType);
+			tx = await registry.deregister(registryId, { from: ownerAddress });
 		});
-		it('should prevent a non-owner from registering', async () => {
-			await expectRevert(
-				registry.register(streetName, postCode, city, country, realtyType, userOne, {
-					from: userTwo,
-				}),
-				'Ownable: caller is not the owner.'
-			);
+		describe('when called by the contract owner', () => {
+			it('should emit a deregistration event', () => {
+				expectEvent(tx, 'RegistrationRemoved', {
+					registryId: registryId,
+				});
+			});
+			it('should update ownerOf state correctly', async () => {
+				assert.equal(await registry.ownerOf.call(registryId), constants.ZERO_ADDRESS);
+			});
+			it('should update totalRegistrations state correctly', async () => {
+				const totalRegistrations = await registry.totalRegistrations.call();
+				assert.equal(totalRegistrations, 0);
+			});
+			it('should update currentID state correctly', async () => {
+				const currentID = await registry.currentID.call();
+				assert.equal(currentID, 1);
+			});
+		});
+		describe('when not called by the contract owner', () => {
+			it('should revert', async () => {
+				await expectRevert(
+					registry.deregister(registryId, {
+						from: userTwo,
+					}),
+					'Ownable: caller is not the owner.'
+				);
+			});
 		});
 	});
 });
